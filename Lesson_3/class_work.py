0b03e01683780fd8ec96aa3d55c56d2ff9f4ecc0
Urnguk <spiri-o-wate@mail.ru>
2025-03-01 14:48:50 +0300

    Create class_work.py

---
+++ Lesson_3/class_work.py
@@ -0,0 +1,19 @@
+N, M = [int(x) for x in input().split()]
+edge_list = []
+for i in range(M):
+    s, f = [int(x) for x in input().split()]
+    edge_list.append((s, f))
+
+adj_matrix = [[0 for j in range(N)] for i in range(N)]
+for edge in edge_list:
+    s, f = edge
+    adj_matrix[s][f] = 1
+    adj_matrix[f][s] = 1
+ 
+adj_list = [[] for i in range(N)]
+for edge in edge_list:
+    s, f = edge
+    adj_list[s].append(f)
+    adj_list[f].append(s)
+
+
