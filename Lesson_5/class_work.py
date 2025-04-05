bc79954af2d5ea67e41ae0b6f91157d4f41b7498
Urnguk <spiri-o-wate@mail.ru>
2025-04-05 16:11:43 +0300

    Update class_work.py

--- Lesson_5/class_work.py
+++ Lesson_5/class_work.py
@@ -7,7 +7,7 @@ def read_adj_list():
         # res[y].append((x, w))
 
 
-def dijkstra(adj_list, start_node):
+def Dijkstra(adj_list, start_node):
     dist = {i: float('inf') for i in range(len(adj_list))}
     dist[start_node] = 0
     res = {}
@@ -23,10 +23,34 @@ def dijkstra(adj_list, start_node):
         dist.pop(min_node)
         
         for adj_node, weight in adj_list[min_node]:
-            if dist[adj_node] > min_dist + weight:
-                dist[adj_node] = min_dist + weight
+            if adj_node in dist:
+                dist[adj_node] = min(min_dist + weight, dist[adj_node])
+
     return res
 
 
+def FordBellman(edge_list, N, start_node):
+    dist = [float("inf") for i in range(N)]
+    dist[start_node] = 0
+    correct = [True for i in range(N)]
+
+    for i in range(2 * N - 1):
+        for j in range(len(edge_list)):
+            s, f, w = edge_list[j]
+            if dist[f] > dist[s] + w:
+                dist[f] = dist[s] + w
+                if i >= N:
+                    correct[f] = False
+
+    return dist, correct
+
+
+def FloydWarshall(adj_matrix):
+    n = len(adj_matrix)
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                adj_matrix[i][j] = min(adj_matrix[i][k] + adj_matrix[k][j], adj_matrix[i][j])
+    return adj_matrix
                 
     
