d7075f2ad52a8a7ede51ff3d73e3887d8db32cd0
Urnguk <spiri-o-wate@mail.ru>
2025-04-19 15:50:50 +0300

    Update class_work.py

--- Lesson_7/class_work.py
+++ Lesson_7/class_work.py
@@ -1,23 +1,54 @@
-def moves(n):
-    res = [n - i for i in (1, 2, 3)]
-    if n % 3 == 0:
-        res.pop()
-    elif n % 3 == 1:
-        res.pop(1)
+# def moves(n):
+#     res = [n - i for i in (1, 2, 3)]
+#     if n % 3 == 0:
+#         res.pop()
+#     elif n % 3 == 1:
+#         res.pop(1)
+#     return [x for x in res if x >= 0]
+#
+#
+# def Grandi(n, cache=None):
+#     if cache is None:
+#         cache = dict()
+#     if n not in cache:
+#         m = moves(n)
+#         if len(m) == 0:
+#             cache[n] = 0
+#         else:
+#             children = set()
+#             for move in m:
+#                 children.add(Grandi(move, cache))
+#             res = n
+#             for i in range(n):
+#                 if i not in children:
+#                     res = i
+#                     break
+#             cache[n] = res
+#     return cache[n]
+#
+#
+# x = int(input())
+# g = Grandi(x)
+# if g > 0:
+#     print(1)
+# else:
+#     print(2)
+
+
+def moves(n, k):
+    res = [n - 1 - i for i in range(k)]
     return [x for x in res if x >= 0]
 
 
-def Grandi(n, cache=None):
-    if cache is None:
-        cache = dict()
+def Grandi(n, k, cache):
     if n not in cache:
-        m = moves(n)
+        m = moves(n, k)
         if len(m) == 0:
             cache[n] = 0
         else:
             children = set()
             for move in m:
-                children.add(Grandi(move, cache))
+                children.add(Grandi(move, k, cache))
             res = n
             for i in range(n):
                 if i not in children:
@@ -27,12 +58,20 @@ def Grandi(n, cache=None):
     return cache[n]
 
 
-x = int(input())
-g = Grandi(x)
-if g > 0:
-    print(1)
-else:
-    print(2)
+def Multi_Grandi(games, k, cache=None):
+    if cache is None:
+        cache = dict()
+    res = Grandi(games[0], k, cache)
+    for i in range(1, len(games)):
+        res ^= Grandi(games[i], k, cache)
+    return res
 
+
+n, k = [int(x) for x in input().split()]
+games = [int(x) for x in input().split()]
+if Multi_Grandi(games, k) > 0:
+    print("YES")
+else:
+    print("NO")
 
 
